7a5596b439c4a74122df7fa662f0a6eafd476d15
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-06-24 20:25:19 +0200

    fix style for index feat add btn link for create page

--- src/main/java/org/lessons/java/pizzeriacurd/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/java/pizzeriacurd/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 
-    List<Pizza> findByNameContainingIgnoreCase(String name);
+    public List<Pizza> findByNameContainingIgnoreCase(String name);
 
 }
