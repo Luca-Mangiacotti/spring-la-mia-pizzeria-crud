6298206e50e884392242e73450e9406341838559
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-06-24 19:35:20 +0200

    feat add control for create new pizza

--- src/main/java/org/lessons/java/pizzeriacurd/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/pizzeriacurd/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -4,10 +4,16 @@ import org.lessons.java.pizzeriacurd.spring_la_mia_pizzeria_crud.repository.Pizz
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.lessons.java.pizzeriacurd.spring_la_mia_pizzeria_crud.model.Pizza;
 import java.util.List;
 
@@ -53,5 +59,21 @@ public class PizzaController {
     public String create(Model model) {
         model.addAttribute("pizza", new Pizza());
         return "pizzas/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza pizzaForm, BindingResult bindingResult, Model model) {
+        // utilizziamo il bindingResult per far verificare al validatore se ci sono
+        // stati errori di inserimento nel form
+        // in caso ci fossero ci sarà un indirizzamento allo stesso form
+        if (bindingResult.hasErrors()) {
+            return "pizzas/create";
+        }
+
+        // nel caso non ci siano errori possiamo salvare il nostro prodotto tramite la
+        // repository con il comando .save
+        repository.save(pizzaForm);
+        return "redirect:/pizzas";
+
     }
 }
