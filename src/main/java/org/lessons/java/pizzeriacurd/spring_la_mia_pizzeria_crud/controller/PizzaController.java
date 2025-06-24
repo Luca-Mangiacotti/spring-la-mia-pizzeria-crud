0ca460598efc77bbf8a7d614c607a53334e45d7a
Luca-Mangiacotti <lucamangiacotti100@gmail.com>
2025-06-24 11:59:59 +0200

    feat add show detail for pizzas

--- src/main/java/org/lessons/java/pizzeriacurd/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/pizzeriacurd/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.lessons.java.pizzeriacurd.spring_la_mia_pizzeria_crud.model.Pizza;
 import java.util.List;
 
@@ -16,7 +18,7 @@ public class PizzaController {
     @Autowired
     private PizzaRepository repository;
 
-    @GetMapping("/index")
+    @GetMapping
     public String index(Model model) {
 
         // con questo comando è come fare una query SELECT * from pizzas e li trasforma
@@ -25,5 +27,19 @@ public class PizzaController {
         model.addAttribute("pizzas", pizzas);
 
         return "pizzas/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+
+        // con questo comando è come fare una query SELECT * from pizzas where "id" =id
+        Pizza pizza = repository.findById(id).get();
+
+        if (pizza == null) {
+            return "rederict:/pizzas/index";
+        }
+        model.addAttribute("pizza", pizza);
+
+        return "pizzas/detail";
     }
 }
